b5017b75fdda9bbaf70561eee051a76dce46d239
Jakub Orzyłowski <s21001@pjwstk.edu.pl>
2021-04-14 18:27:56 +0200

    v 0.2

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Helo World!"}
+
+@app.get("/hello/{name}")
+def hello_name(name: str):
+    return f"Hello {name}"
--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 from main import app
 
@@ -8,3 +9,9 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Helo World!"}
+
+@pytest.mark.parametrize("name",["Zenek", "Marta"])
+def hello_name_param(name):
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.text == f'"Helo {name}'
