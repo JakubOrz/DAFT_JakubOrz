c627b41fee943e5cd53dfd79bc941307ce44bf6b
Jakub Orzyłowski <s21001@pjwstk.edu.pl>
2021-04-30 23:07:11 +0200

    v 0.3.4

--- zad3.py
+++ zad3.py
@@ -10,7 +10,9 @@ zadanie3ruter = APIRouter()
 kluczeSesji: set = set()
 secretKey = "Tajnekoddostarwarsilubieplackiivieratobyladobraadmin"
 security = HTTPBasic()
-realpassowrd = "4dm1n:NotSoSecurePa$$"
+realpassowrd2 = "4dm1n:NotSoSecurePa$$"
+realusername = "4dm1n"
+realpassword = "NotSoSecurePa$$"
 
 
 @zadanie3ruter.get("/test")
@@ -23,20 +25,28 @@ def zadanie3_1():
     return HTMLResponse(content=f"<h1>Hello! Today date is {date.today()}</h1>", status_code=200)
 
 
+@zadanie3ruter.post("/logintest")
+def logintest(response: Response, logintoken: HTTPBasicCredentials = Depends(security)):
+    print(logintoken)
+    return logintoken
+
+
 @zadanie3ruter.post("/login_session")
-def loginSession(response: Response, logintoken: HTTPBasicCredentials = Depends(security)):
-    if base64.b64decode(logintoken).decode("Utf-8") != realpassowrd:
-        raise HTTPException(status_code=401, detail="Unathorized")
-    session_token = sha256(f"{secretKey}{realpassowrd}".encode()).hexdigest()
+def loginSession(response: Response, logintoken: HTTPBasicCredentials = Depends(security),
+                 status_code=status.HTTP_201_CREATED):
+    if str(logintoken.username) != realusername or str(logintoken.password) != realpassword:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    session_token = sha256(f"{secretKey}{date.today()}".encode()).hexdigest()
     kluczeSesji.add(session_token)
     response.set_cookie(key="token", value=session_token)
-    return
+    response.status_code = status.HTTP_201_CREATED
+    return response
 
 
 @zadanie3ruter.post("/login_token")
 def loginToken(*, logintoken: HTTPBasicCredentials = Depends(security), token=Cookie(None)):
-    if base64.b64decode(logintoken).decode("Utf-8") != realpassowrd:
-        raise HTTPException(status_code=401, detail="Unathorized")
+    if str(logintoken.username) != realusername or str(logintoken.password) != realpassword:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     if token in kluczeSesji:
         return {"token": str(token)}
-    raise HTTPException(status_code=401, detail="Unathorised")
+    raise HTTPException(status_code=401)
